433906c3135ba4b5790377681fa098779ec7059e
hackerMao <hacker_murray@163.com>
2022-01-17 23:25:14 +0800

    modify interface example

--- interface/main.go
+++ interface/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+/**
+结构体方法接收指针，则可将结构体指针对象赋值给接口，接口边可以直接调用方法
+结构体方法接受值类型，既可以将指针也可以将值类型赋值给接口，因为在使用值类型时go会自动使用引用，这在go里算是一个语法糖
+值接受者会自动生成指针接受者，所以值对象和指针对象都可以赋值给接口，而指针接受者则不能
+*/
+
 type Sender interface {
 	Send(to string, msg string) error
 	SendAll(to []string, msg string) error
@@ -11,7 +17,7 @@ type EmailSender struct {
 }
 
 func (e *EmailSender) Send(to string, msg string) error {
-	fmt.Printf("发送邮件到：%s, 邮件内容：%s\n", to, msg)
+	fmt.Printf("发送邮件到: %s, 邮件内容: %s\n", to, msg)
 	return nil
 }
 
